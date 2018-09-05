6d2be637d4f1bb56d101ba71106d7075f3a87bc4
timtingwei <timtingwei@hotmail.com>
2018-09-05 23:39:53 +0800

    Update 2097-sky.cpp

--- HDU/2097-Sky数.cpp
+++ HDU/2097-Sky数.cpp
@@ -3,47 +3,6 @@
 #include <cstdio>
 using namespace std;
 
-/*
-int main() {
-  int num;
-  while (scanf("%d", &num) && num) {
-    int a = num, b = num, c = num, r1 = 0, r2 = 0, r3 = 0;
-    int ok = 1, sky = 0;
-    while (a) {
-      r1 += a % 10;
-      a /= 10;
-    }
-    sky = r1;
-    // if (r1 == 22) {   // debug01
-    if (r1 == sky) {
-      while (b) {
-        r2 += b % 16;
-        b /= 16;
-      }
-      if (r2 == sky) {
-        while (c) {
-          r3 += c % 12;
-          c /= 12;
-        }
-        if (r3 != sky) {
-          ok = 0;
-        }
-      } else {
-        ok = 0;
-      }
-    } else {
-      ok = 0;
-    }
-    if (ok) {
-      printf("%d is a Sky Number.\n", num);
-    } else {
-      printf("%d is not a Sky Number.\n", num);
-    }
-  }
-  return 0;
-}
-*/
-
 int main() {
   int n = 0;
   while (scanf("%d", &n) && n) {
@@ -53,6 +12,17 @@ int main() {
       sum /= 10;
     }
     sum = n;
+    while (sum) {
+      b += sum % 16;
+      sum /= 16;
+    }
+    sum = n;
+    while (sum) {
+      c += sum % 12;
+      sum /= 12;
+    }
+    (a == b && b == c) ? printf("%d is a Sky Number.\n", n) :
+        printf("%d is not a Sky Number.\n", n);
   }
   return 0;
 }
