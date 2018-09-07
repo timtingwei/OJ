df2ce34cdc69072893b11b7f887c76b471c28b59
timtingwei <timtingwei@hotmail.com>
2018-09-07 13:01:41 +0800

    Update 1005-number-sequence.cpp

--- HDU/1005-number-sequence.cpp
+++ HDU/1005-number-sequence.cpp
@@ -1,24 +1,5 @@
 // Copyright [2018] <mituh>
 // 1005-number-sequence.cpp
-/*
-#include <cstdio>
-
-int f(int n, int a, int b) {
-  if (n == 1 || n == 2) return 1;
-  return (a * f(n-1, a, b) + b * f(n-2, a, b)) % 7;
-}
-
-int main() {
-  int a, b, n;
-  while (scanf("%d %d %d", &a, &b, &n) != EOF && a && b && n) {
-    printf("%d\n", f(a, b, n));
-  }
-  return 0;
-}
-
-// TLE
-
-*/
 
 /*
 #include <cstdio>
@@ -39,6 +20,7 @@ int main() {
 // MLE
 */
 
+/*
 #include <cstdio>
 
 int main() {
@@ -61,7 +43,33 @@ int main() {
 
 
 // TLE
+*/
 
+
+
+
+
+// 打表, 求余
+#include <cstdio>
+int main() {
+  int a, b, n;
+  while (scanf("%d %d %d", &a, &b, &n)) {
+    int i;
+    int seq[1000] = {0};
+    seq[1] = 1;
+    seq[2] = 1;
+    int t;
+    if (a == 0 && b == 0 && n == 0) {
+      break;
+    }
+    for (i = 3; i <= 49; i++) {
+      seq[i] = (a * seq[i-1] + b * seq[i-2]) % 7;
+    }
+    t = n % 49;
+    printf("%d\n", seq[t]);
+  }
+  return 0;
+}
 
 
 
